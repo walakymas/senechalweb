d9837ade63b12a047240bfa7d752eba403bb9104
István Svorcz <walakymas@gmail.com>
2021-03-21 17:07:12 +0100

    webhook

--- web/static/team.js
+++ web/static/team.js
@@ -35,12 +35,14 @@ function addBlock (id, func, def) {
     keys.forEach(function (k) {
         s=''
         for (i in pcs) {
-            s+='<td>'
-            s+=func(i)[k] ? func(i)[k] : def;
-            s+='</td>'
+            v = func(i)[k]
+            if (v) {
+                s+='<td  bot="!c '+k.replace(' ','_')+' <@!'+pcs[i]['memberId']+'>">'+v+'</td>'
+            } else {
+                s+='<td >'+def+'</td>'
+            }
         }
-        console.log(s)
-        $('#'+id).append('<tr added="'+id+'"><th>'+k+'</th>'+s+'</tr>')
+        $('#'+id).append('<tr added="'+id+'"  bot="!team '+k.replace(' ','_')+'"><th>'+k+'</th>'+s+'</tr>')
     })
 
 }
@@ -49,26 +51,43 @@ function addBlock (id, func, def) {
 function redraw() {
     for (i in pcs ) {
         console.log(i+':'+pcs[i]['shortName'])
-        $('#name').append('<th added="name">'+pcs[i]['shortName']+'</th>')
+        $('.name').append('<th added="name">'+pcs[i]['shortName']+'</th>')
         $('#siz').append('<td added="siz">'+pcs[i]['stats']['siz']+'</td>')
         $('#dex').append('<td added="dex">'+pcs[i]['stats']['dex']+'</td>')
         $('#con').append('<td added="con">'+pcs[i]['stats']['con']+'</td>')
         $('#str').append('<td added="str">'+pcs[i]['stats']['str']+'</td>')
         $('#app').append('<td added="app">'+pcs[i]['stats']['app']+'</td>')
+        
+        $('#dam').append('<td added="dam">'+Math.round((pcs[i]['stats']['str']*1+pcs[i]['stats']['siz']*1)/6)+'</td>')
+        $('#hr').append('<td added="hr">'+Math.round((pcs[i]['stats']['str']*1+pcs[i]['stats']['con']*1)/10)+'</td>')
+        $('#mr').append('<td added="mr">'+Math.round((pcs[i]['stats']['dex']*1+pcs[i]['stats']['siz']*1)/10)+'</td>')
+        $('#hp').append('<td added="hp">'+Math.round((pcs[i]['stats']['con']*1+pcs[i]['stats']['siz']*1))+'</td>')
+        $('#unc').append('<td added="unc">'+Math.round((pcs[i]['stats']['con']*1+pcs[i]['stats']['siz']*1)/4)+'</td>')
+        $('#mw').append('<td added="mw">'+pcs[i]['stats']['con']+'</td>')
+        $('#kno').append('<td added="kno">'+pcs[i]['stats']['siz']+'</td>')
+             
     }
     for (t in traits) {
 
         tn = traits[t][0].substr(0,3).toLowerCase();
-        s =  ''
+        s0 =  ''
+        s1 =  ''
         for (i in pcs ) {
-            s+='<td>'+pcs[i]['traits'][tn]+' / '+(20-pcs[i]['traits'][tn]*1)+'</td>'
+            s0+='<td bot="!c '+traits[t][0].replace(' ','_')+' <@!'+pcs[i]['memberId']+'>">'+pcs[i]['traits'][tn]+'</td>'
+            s1+='<td bot="!c '+traits[t][1].replace(' ','_')+' <@!'+pcs[i]['memberId']+'>">'+(20-pcs[i]['traits'][tn]*1)+'</td>'
         }
-        $('#traits').append('<tr added="trait_'+tn+'"><th>'+traits[t][0]+' / '+traits[t][1]+'</th>'+s+'</tr>')
+        $('#traits').append('<tr added="trait_'+tn+'0" bot="!team '+traits[t][0].replace(' ','_')+'"    ><th>'+traits[t][0]+'</th>'+s0+'</tr>')
+        $('#traits').append('<tr added="trait_'+tn+'1" bot="!team '+traits[t][1].replace(' ','_')+'"><th>'+traits[t][1]+'</th>'+s1+'</tr>')
     }
     addBlock('combat', function(i) { return pcs[i]['skills']['Combat']},'--')
-    addBlock('weapons', function(i) { return pcs[i]['skills']['Weapons']},'--')
+    addBlock('combat', function(i) { return pcs[i]['skills']['Weapons']},'--')
     addBlock('other', function(i) { return pcs[i]['skills']['Other']},'--')
     addBlock('passions', function(i) { return pcs[i]['passions']},'--')
+    $('[bot]').click(function(){
+        $.post('https://discord.com/api/webhooks/822974917816483870/J5FB3eGrDP1xJ9FYeZaVIISz8YEHEl2b7rLZN8i0sc1OE4NQ-DVh41e_DUH3Saw7LabU',
+         {username: 'WebHook', content:$(this).attr('bot')})
+    })
+
 }
 
   $( function() {
@@ -76,4 +95,5 @@ function redraw() {
         pcs = list
         redraw()
       });
+//
   });
